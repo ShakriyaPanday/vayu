61daadb52a425ffda500f666cc25949ce0cfb1a2
Shakriya Panday <112616304+ShakriyaPanday@users.noreply.github.com>
2025-01-24 01:27:28 +0545

    Update new.py

--- new.py
+++ new.py
@@ -16,7 +16,7 @@ app.title = "VayuDrishti Dashboard"
 server = app.server
 
 # Load pollution data from CSV
-dmap = pd.read_csv("/kaggle/input/global-air-pollution-dataset/global air pollution dataset.csv")
+dmap = pd.read_csv("https://raw.githubusercontent.com/ShakriyaPanday/vayu/refs/heads/main/pollution.csv")
 # Footer
 footer = dbc.Container(
     html.Footer(
